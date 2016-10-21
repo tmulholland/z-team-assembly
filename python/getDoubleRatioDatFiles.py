d9d7cb7f7aeaa6ffac826241503196eaad47391f
Troy <mulholland@pizero.colorado.edu>
2016-10-20 22:06:37 -0600

    Add first set of python scripts needed to make data inputs and root files

---
+++ python/getDoubleRatioDatFiles.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+"""
+getDoubleRatioDatFiles.py generates the double ratio
+dat files for integration. Can be run from
+command line, interactively or with the driver script:
+scripts/extrapDatFileDriver.csh
+"""
+import sys
+import ROOT
+import RA2b
+
+## doSample is a list of dat files to generate ##
+doSample = []
+## Check if samples are given at runtime (e.g. sig hdp ldp)
+## can give more than one and will run over each sequentially
+if len(sys.argv) > 1:
+    for i in range(1,len(sys.argv)):
+        doSample.append(sys.argv[i])
+else:
+    ## default runs over all 
+    doSample = ['sig','hdp','ldp']
+#################################################
+
+for sample in doSample:
+
+    ## no 9+ jets events in ldp sample
+    if(sample=='ldp'):
+        nj_binning = [1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5]
+    else:
+        nj_binning = [1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5]
+
+    ## include sideband if ldp or hdp
+    if(sample=='ldp' or sample=='hdp'):
+        mht_binning = [250.,300.,350.,400.,450.,600.,750,900.]
+        mhtCut = False
+    else:
+        mhtCut = True
+        mht_binning = [300.,350.,400.,450.,600.,750,900.]
+
+    ## get the double ratio graphs
+    nj_dr = RA2b.getDoubleRatioGraph('NJets',applyPuWeight=True,dphiCut=sample,binning=nj_binning,applyMHTCut=mhtCut)
+    mht_dr = RA2b.getDoubleRatioGraph('MHT',applyPuWeight=True,dphiCut=sample,binning=mht_binning,applyMHTCut=mhtCut)
+    ht_dr = RA2b.getDoubleRatioGraph('HT',applyPuWeight=True,dphiCut=sample,applyMHTCut=mhtCut)
+        
+    ## get the double ratio plots with values and uncertainties
+    dr_out = RA2b.getDoubleRatioPlot([nj_dr,mht_dr,ht_dr])
+    
+    ## define kinematic range ##
+    kinRange = [] 
+    ## if qcd binning add 11-13 to beginning of kinRange
+    if(sample=='ldp' or sample=='hdp'):
+        kinRange+=range(11,14)
+    kinRange+=range(1,11)
+
+    ## define the dictionaries to transcribe the 
+    ## double ratio bins to the analysis bins    
+    mhtDict = {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0,
+            6: 0,
+            7: 1,
+            8: 1,
+            9: 1,
+            10: 2,
+            11: 4,
+            12: 4,
+            13: 4,
+            -1: 0,
+    }
+    htDict = {
+            1: 0,
+            2: 1,
+            3: 2,
+            4: 0,
+            5: 1,
+            6: 2,
+            7: 3,
+            8: 1,
+            9: 2,
+            10: 1,
+            11: 2,
+            12: 4,
+            13: 5,
+            -1: 0,
+    }
+    
+    dr = dr_out[0][0]
+    edr = dr_out[0][1]
+    
+    effFile = ROOT.TFile("../plots/histograms/effHists.root")
+        
+    h_pur_m = effFile.Get("h_pur_m")
+    h_pur_e = effFile.Get("h_pur_e")
+    h_trig_m = effFile.Get("h_trig_m1")    
+    h_trig_e = effFile.Get("h_trig_e1")    
+    
+    ## hard code the btag SF and error for now
+    btagSF = 1.
+    btagSFerror = 0.005
+    
+    ## hard coded lepton SFs
+    lepSF = 0.95
+    eLepSF = 0.05
+    
+    ## trigger
+    trig = (h_trig_m.GetBinContent(1)+h_trig_e.GetBinContent(1))/2.
+    trigError = (h_trig_m.GetBinError(1)+h_trig_e.GetBinError(1))/2.
+    
+    ## purity just take 3-4 jet 0b
+    pur = (h_pur_m.GetBinContent(4)+h_pur_e.GetBinContent(4))/2.
+    purError = (h_pur_m.GetBinError(4)+h_pur_e.GetBinError(4))/2.
+        
+    ## had to add this funny business to 
+    ## remove the bins that don't have
+    ## any events
+    removedBins = RA2b.getRemovedBins(nbRange=0,kinRange=kinRange)
+    subtractBins = removedBins[0]
+    avoidBins = removedBins[1]
+    uncutBin = 0
+    Bin = 1
+    
+    print "DR | DR cv error | DR shape up | DR shape down | 0b purity | purity error | trig eff | trig eff error | lepton SF | lepton SF error | btag SF | btag SF error |"
+    for nj in range(1,6):
+        for kin in kinRange:
+            uncutBin+=1
+            if(uncutBin in avoidBins):
+                continue
+            mht = mhtDict[kin]
+            ht = htDict[kin]
+            EdrUp = max(dr_out[1]['NJets'][min(max(nj,1),5)-1][0], dr_out[1]['HT'][ht][0], dr_out[1]['MHT'][mht][0])
+            EdrUp = (max(EdrUp**2-edr**2,0))**0.5
+            EdrDown = max(dr_out[1]['NJets'][min(max(nj,1),5)-1][1], dr_out[1]['HT'][ht][1], dr_out[1]['MHT'][mht][1])
+            EdrDown = (max(EdrDown**2-edr**2,0))**0.5
+            print str(dr)+" | "+str(edr)+" | "+str(EdrUp)+" | "+str(EdrDown)+" | "+str(pur)+" | "+str(purError/pur)+" | "+str(trig)+" | "+str(trigError/trig)+" | "+str(lepSF)+" | "+str(eLepSF)+" | "+str(btagSF)+" | "+str(btagSFerror) + " | "
+            Bin+=1
+
---
+++ python/getExtrapDatFiles.py
@@ -0,0 +1,216 @@
+#!/usr/bin/python
+"""
+getExtrapDatFiles.py generates the extrapolation 
+dat files for integration. Can be run from
+command line, interactively or with the driver script:
+scripts/extrapDatFileDriver.csh
+"""
+import sys
+import ROOT
+import RA2b
+
+## doSample is a list of dat files to generate ##
+doSample = []
+## Check if samples are given at runtime (e.g. sig hdp ldp)
+## can give more than one and will run over each sequentially
+if len(sys.argv) > 1:
+    for i in range(1,len(sys.argv)):
+        doSample.append(sys.argv[i])
+else:
+    ## default runs over all 
+    doSample = ['sig','hdp','ldp']
+#################################################
+
+for sample in doSample:
+
+    ## define kinematic range ##
+    kinRange = [] 
+    ## if qcd binning add 11-13 to beginning of kinRange
+    if(sample=='ldp' or sample=='hdp'):
+        kinRange+=range(11,14)
+
+    ## all samples use 10 nominal kinematic bins
+    kinRange+=range(1,11)
+    ############################
+
+    ## the individual dilepton yields per bin
+    zee = RA2b.getHist('zee',dphiCut=sample,kinRange=kinRange)
+    zmm = RA2b.getHist('zmm',dphiCut=sample,kinRange=kinRange)
+    
+    ## the stat uncertainty on the extrapolation factors
+    zll_Extrap = RA2b.getExtrapolation(['zee','zmm'],njSplit=False,doFactorization=True,kinRange=-1,dphiCut=sample)
+    statErr = []
+    for i in range(1,20):
+        if(zll_Extrap.GetBinContent(i)==0):
+            statErr.append(1.)
+        else:
+            statErr.append(zll_Extrap.GetBinError(i)/zll_Extrap.GetBinContent(i))
+    
+    ####################################################################
+    ## get the purity uncertainties from the effHists.root file ########
+    ####################################################################
+    effFile = ROOT.TFile("../plots/histograms/effHists.root")
+    
+    h_pur_m = effFile.Get("h_pur_m")
+    h_pur_e = effFile.Get("h_pur_e")
+    
+    ## NJets=2 purity as a function of nb
+    pursyst_2j = [0]
+    for i in range(2,4):
+        avePur = (h_pur_e.GetBinError(i)/h_pur_e.GetBinContent(i)+h_pur_m.GetBinError(i)/h_pur_m.GetBinContent(i))/2
+        pursyst_2j.append(avePur)
+
+    ## NJets=3-4 purity as a function of nb
+    pursyst_3to4j = [0]
+    for i in range(5,8):
+        avePur = (h_pur_e.GetBinError(i)/h_pur_e.GetBinContent(i)+h_pur_m.GetBinError(i)/h_pur_m.GetBinContent(i))/2
+        pursyst_3to4j.append(avePur)
+    
+    ## NJets=5+ purity as a function of nb
+    pursyst_5jplus = [0]
+    for i in range(9,12):
+        avePur = (h_pur_e.GetBinError(i)/h_pur_e.GetBinContent(i)+h_pur_m.GetBinError(i)/h_pur_m.GetBinContent(i))/2
+        pursyst_5jplus.append(avePur)
+    
+    ## comine the uncertainties into one list to be called later
+    purSyst = [pursyst_2j,pursyst_3to4j,pursyst_5jplus,pursyst_5jplus,pursyst_5jplus]
+  
+    ####################################################################
+    ## end get purity uncertainties ####################################
+    ####################################################################
+
+    ## get the 0b photon data
+    ## this is needed to do the split and merge type extrapolation
+    ## using hdp even for ldp because it gets divided out
+    ## and we gain from the increased stats 
+    h_pho0b_split = RA2b.get0bPrediction('photon',kinRange=kinRange)
+    h_pho0b_merged = RA2b.mergeHist(h_pho0b_split,kinRange=kinRange)
+    
+    ## get the extrapolation factors from data with purities applied
+    h_zeemmExtrp = RA2b.getExtrapolation(['zee','zmm'],doFactorization=True,applyEffs=True,dphiCut=sample,kinRange=kinRange)
+
+    ## clone the data extrapolation factors before MC correction for syst calc
+    h_zeemmNoCorr = h_zeemmExtrp.Clone()
+    
+    ## get the correction factors from MC
+    dy_mc_corr = RA2b.getMonteCarloCorrection(['dyee','dymm'],applySF=False,applyPuWeight=True,dphiCut=sample,kinRange=kinRange)
+    dyll_mc_corr = RA2b.getMonteCarloCorrection(['dyee','dymm'],njSplit=False,kinRange=-1,applySF=False,applyPuWeight=True)
+    
+    ## get the binomial correction for syst calc
+    h_binom=RA2b.getBinomialCorrection(kinRange=kinRange)
+    
+    ## apply the mc correction factors 
+    h_zeemmExtrp.Multiply(dy_mc_corr)
+    
+    # to compute up and low syst
+    h_zeemmExtrpNoTTZ = h_zeemmExtrp.Clone() 
+    
+    ## apply the binomial correction factors for syst calc
+    h_binom.Multiply(h_zeemmNoCorr)
+    
+    ## clone the split photon sample as place holders for syst calc 
+    h_pho0b_binom = h_pho0b_split.Clone()
+    h_pho0b_nocorr = h_pho0b_split.Clone()
+    h_pho0b_mid = h_pho0b_split.Clone()
+
+    ## multiply by the various extrapolation factors
+    h_pho0b_split.Multiply(h_zeemmExtrp)
+    h_pho0b_mid.Multiply(h_zeemmExtrpNoTTZ)
+    h_pho0b_binom.Multiply(h_binom)
+    h_pho0b_nocorr.Multiply(h_zeemmNoCorr)
+    
+    ## merge the hists back to the 174 binning scheme
+    h_pho_merged = RA2b.mergeHist(h_pho0b_split,kinRange=kinRange)
+    h_pho_mid = RA2b.mergeHist(h_pho0b_mid,kinRange=kinRange)
+    h_pho_upsyst = RA2b.mergeHist(h_pho0b_binom,kinRange=kinRange)
+    h_pho_nocorr = RA2b.mergeHist(h_pho0b_nocorr,kinRange=kinRange)
+    
+    ## compute the upper and lower syst hists
+    h_pho_dnsyst = h_pho_mid.Clone()
+    h_pho_upsyst.Add(h_pho_mid,-1)
+    h_pho_dnsyst.Add(h_pho_nocorr,-1)
+    
+    ## divide mid to get fractional error
+    h_pho_upsyst.Divide(h_pho_mid)
+    h_pho_dnsyst.Divide(h_pho_mid)
+    
+    ## scale by 1/sqrt(3) : full width at half max
+    h_pho_upsyst.Scale(1/3.**0.5)
+    h_pho_dnsyst.Scale(1/3.**0.5)
+    
+    ## divide out the photon normalization
+    ## this gives the extrapolation factors themselves
+    h_extrap = h_pho_merged.Clone()
+    h_extrap.Divide(h_pho0b_merged)
+    
+    ## kin systematics derived using
+    ## analysis of the closure plot
+    systKin = [0,0.07,0.10,0.20]
+    
+    ## formatting dictionaries 
+    njDict = {1: ' 2 ',
+              2: '3-4',
+              3: '5-6',
+              4: '7-8',
+              5: '9+ '}
+    nbDict = {0: '  0   ',
+              1: '  1   ',
+              2: '  2   ',
+              3: ' >=3  '}
+    
+    ## this block is here to ensure that even when the corresponding
+    ## photon 0b bin is zero, the extrapolation factors are non zero
+
+    ## first NJets=9 bin
+    Bin = 4*4*len(kinRange)-4*(len(kinRange)+2)/5-len(kinRange)+1
+
+    ## remove  low HT bins
+    kinRangeCut = kinRange
+    for i in [11,1,4]:
+        if i in kinRangeCut:
+            kinRangeCut.remove(i)
+
+    nonZeroBin = Bin-len(kinRangeCut)
+    for nj in [5]:
+        for nb in range(4):
+            nonZeroBin += len(kinRangeCut)
+            for kin in kinRangeCut:
+                h_extrap_9p = h_extrap.GetBinContent(nonZeroBin)
+                h_upsyst_9p = h_pho_upsyst.GetBinContent(nonZeroBin)
+                h_dnsyst_9p = h_pho_dnsyst.GetBinContent(nonZeroBin)
+                h_extrap.SetBinContent(Bin,h_extrap_9p)
+                h_pho_upsyst.SetBinContent(Bin,h_upsyst_9p)
+                h_pho_dnsyst.SetBinContent(Bin,h_dnsyst_9p)
+                Bin+=1
+
+    
+    ## had to add this funny business to 
+    ## remove the bins that don't have
+    ## any events
+    removedBins = RA2b.getRemovedBins()
+    subtractBins = removedBins[0]
+    avoidBins = removedBins[1]
+    
+    uncutBin = 0
+    Bin = 1
+    nbnjSubtract = 0
+    print "Njbin | Nbbin | Nmumu |  Nee  | Nb/0b   | stat | MC stat | syst+ | syst- | sysKin | sysPur"
+    for nj in range(1,6):
+        for nb in range(4):
+            if(nj==1 and nb==3):
+                nbnjSubtract+=1
+            for kin in kinRange:
+                uncutBin+=1
+                if(uncutBin in avoidBins):
+                    continue
+                line = " "+njDict[nj]+"  |  "+nbDict[nb]+"  |   "
+                line+= str(zmm.GetBinContent(Bin))+"  |"+str(zee.GetBinContent(Bin))
+                line+= " | "+str(h_extrap.GetBinContent(Bin))
+                line+= " | "+str(statErr[(nj-1)*4+nb-nbnjSubtract])
+                line+= " | "+str(dyll_mc_corr.GetBinError((nj-1)*4+nb+1-nbnjSubtract)/max(dyll_mc_corr.GetBinContent((nj-1)*4+nb+1-nbnjSubtract),1))
+                line+= " | "+str(abs(h_pho_upsyst.GetBinContent(Bin)))
+                line+= " | "+str(abs(h_pho_dnsyst.GetBinContent(Bin)))
+                line+= " | "+str(systKin[nb])
+                line+= " | "+str(purSyst[nj-1][nb])
+                print line
+                Bin+=1
