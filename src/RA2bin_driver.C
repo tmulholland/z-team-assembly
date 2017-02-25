e870e46587d2421a1191603755c09dd5b85172d8
William T. Ford <wtford@colorado.edu>
2017-02-24 19:25:46 -0700

    Update reference to the MC root file and lumi ratio

--- src/RA2bin_driver.C
+++ src/RA2bin_driver.C
@@ -2,9 +2,9 @@
   gROOT->Reset();
   gROOT->ProcessLine(".L RA2bin_inputs_Zinv.C+");
 
-  RA2bin_inputs_Zinv(LDP, "../datFiles/gJets", "../datFiles/DR", "../datFiles/DY", "../plots/histograms/ZinvMCttzMC160bin.root", 1.0, true);
-  RA2bin_inputs_Zinv(HDP, "../datFiles/gJets", "../datFiles/DR", "../datFiles/DY", "../plots/histograms/ZinvMCttzMC160bin.root", 1.0, true);
-  RA2bin_inputs_Zinv(Signal, "../datFiles/gJets", "../datFiles/DR", "../datFiles/DY", "../plots/histograms/ZinvMCttzMC174bin.root", 1.0, true);
+  RA2bin_inputs_Zinv(LDP, "../datFiles/gJets", "../datFiles/DR", "../datFiles/DY", "../plots/histograms/ZinvMCttzMC174bin.root", 35.9/36.3, true);
+  RA2bin_inputs_Zinv(HDP, "../datFiles/gJets", "../datFiles/DR", "../datFiles/DY", "../plots/histograms/ZinvMCttzMC174 bin.root", 35.9/36.3, true);
+  RA2bin_inputs_Zinv(Signal, "../datFiles/gJets", "../datFiles/DR", "../datFiles/DY", "../plots/histograms/ZinvMCttzMC174bin.root", 35.9/36.3, true);
 
   
 }
